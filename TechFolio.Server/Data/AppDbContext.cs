559ec2026a0faa00b3a10551d6e00a082cf17b49
GoshoPetrov <146186451+GoshoPetrov@users.noreply.github.com>
2025-04-19 11:04:27 +0300

    The DB with Stundents and Projects

---
+++ TechFolio.Server/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechFolio.Data.Models;
+
+
+namespace TechFolio.Server.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Students> Students { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+}
+
